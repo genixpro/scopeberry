490802e784f40b5d82fc1a4aca521dd534047cbd
Bradley Arsenault <genixpro@gmail.com>
2023-08-16 11:33:56 -0400

    Couple of tweaks and fixes to the deployment configuration

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+export function computeAPIUrlPrefix() {
+    if (window.location.href.includes("localhost") || window.location.href.includes("127.0.0.1")) {
+        return "http://localhost:5000";
+    } else {
+        return window.location.href + "/api";
+    }
+}
 
 
 
@@ -8,7 +15,7 @@ export async function getCompletion(text) {
         text: text
     }
 
-    const response = await axios.get("http://localhost:5000/completion", {
+    const response = await axios.get(`${computeAPIUrlPrefix()}/completion`, {
         params: queryParams
     });
 
