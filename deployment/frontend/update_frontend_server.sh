e58d8206dfa5a4d089f1d998e06c6bef37f07b8c
Bradley Arsenault <genixpro@gmail.com>
2023-08-16 09:59:20 -0400

    First pass on implementing a deployment configuration

---
+++ deployment/frontend/update_frontend_server.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1090
+source deployment/environments/$PLANGURU_ENV.sh
+
+
+kubectl apply -f deployment/priorities/default.yaml
+kubectl apply -f deployment/priorities/$PLANGURU_ENV.yaml
+
+sed "s/__REVISION_ID__/$REVISION_ID/g;s/__PLANGURU_ENV__/$PLANGURU_ENV/g;s/__MAX_SURGE__/$MAX_SURGE/g;s/__MAX_UNAVAILABLE__/$MAX_UNAVAILABLE/g" deployment/frontend/frontend_server_deployment.yaml > frontend_server_deployment_$PLANGURU_ENV.yaml
+kubectl apply -f frontend_server_deployment_$PLANGURU_ENV.yaml
+
+sed "s/__REVISION_ID__/$REVISION_ID/g;s/__PLANGURU_ENV__/$PLANGURU_ENV/g;s/__MINIMUM_REPLICATIONS__/$MINIMUM_REPLICATIONS/g;s/__MAXIMUM_REPLICATIONS__/$MAXIMUM_REPLICATIONS/g" deployment/frontend/frontend_autoscaler.yaml > frontend_autoscaler_$PLANGURU_ENV.yaml
+kubectl apply -f frontend_autoscaler_$PLANGURU_ENV.yaml
+
+sed "s/__REVISION_ID__/$REVISION_ID/g;s/__PLANGURU_ENV__/$PLANGURU_ENV/g" deployment/frontend/frontend_service.yaml > frontend_service_$PLANGURU_ENV.yaml
+kubectl apply -f frontend_service_$PLANGURU_ENV.yaml
+
